543feaa99c24c03f75078a314adf7e2b2b7f4199
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-05-28 17:36:06 +0100

    Test JSX

--- app/src/index.js
+++ app/src/index.js
@@ -3,4 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello, world!</h1>);
+
+function format_name(user) {
+    return user.first_name + " " + user.last_name;
+}
+
+const user = {
+    first_name: "f",
+    last_name: "word",
+};
+
+const element = <h1>Hello, {format_name(user)}</h1>;
+root.render(element);

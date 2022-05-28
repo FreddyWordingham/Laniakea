af4106922cca03d6678aea85dc7f4a3c050a7f00
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-05-28 20:01:10 +0100

    Simplified components

--- app/src/clock.tsx
+++ app/src/clock.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 
 type Props = {};
-type State = { date: Date; counter: number };
+type State = { date: Date };
 
 export class Clock extends React.Component<Props, State> {
     timer_id: number;
 
     constructor(props) {
         super(props);
-        this.state = { date: new Date(), counter: 0 };
+        this.state = { date: new Date() };
     }
 
     componentDidMount() {
@@ -20,20 +20,13 @@ export class Clock extends React.Component<Props, State> {
     }
 
     tick() {
-        let x = 0;
-        if (Math.random() > 0.5) {
-            x += 1;
-        }
-
-        this.setState({ date: new Date(), counter: this.state.counter + x });
+        this.setState({ date: new Date() });
     }
 
     render() {
         return (
             <div>
-                <h1>Hello, world!</h1>
-                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-                <h2>Counter: {this.state.counter}.</h2>
+                <h2>{this.state.date.toLocaleTimeString()}</h2>
             </div>
         );
     }

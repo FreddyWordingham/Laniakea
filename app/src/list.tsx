8fe309ba0ce792d8e56ec65acc6b050383e3847a
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-05-29 15:34:47 +0100

    Added List keys

--- app/src/list.tsx
+++ app/src/list.tsx
@@ -1,7 +1,5 @@
 export function List(props) {
     const numbers = props.numbers;
-
-    // const list_items = numbers.map((n) => <li key={n.toString()}>{n}</li>);
-    const list_items = numbers.map((n) => <li>{n}</li>);
+    const list_items = numbers.map((n) => <li key={n.toString()}>{n}</li>);
     return <ul>{list_items}</ul>;
 }

31e5fb7eed65a8bdc1d21cecd7f50c826dcee75f
Freddy Wordingham <fjmw201@exeter.ac.uk>
2022-05-29 16:41:27 +0100

    Improved style of Select component

--- app/src/select.tsx
+++ app/src/select.tsx
@@ -1,19 +1,41 @@
 import React from "react";
 
 type Props = {};
-type State = {};
+type State = { value: string };
 
 export class Select extends React.Component<Props, State> {
+    constructor(props) {
+        super(props);
+        this.state = { value: "coconut" };
+    }
+
+    handle_change = (event) => {
+        this.setState({ value: event.target.value });
+    };
+
+    handle_submit = (event) => {
+        alert("Your favorite flavor is: " + this.state.value);
+        event.preventDefault();
+    };
+
     render() {
         return (
-            <select>
-                <option value="grapefruit">Grapefruit</option>
-                <option value="lime">Lime</option>
-                <option selected value="coconut">
-                    Coconut
-                </option>
-                <option value="mango">Mango</option>
-            </select>
+            <form onSubmit={this.handle_submit}>
+                <label>
+                    Pick your favorite flavor:
+                    <select
+                        value={this.state.value}
+                        onChange={this.handle_change}
+                    >
+                        {" "}
+                        <option value="grapefruit">Grapefruit</option>
+                        <option value="lime">Lime</option>
+                        <option value="coconut">Coconut</option>
+                        <option value="mango">Mango</option>
+                    </select>
+                </label>
+                <input type="submit" value="Submit" />
+            </form>
         );
     }
 }
